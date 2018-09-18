9532fa85df5fbc1f217e9fe6230b9b280248687c
Spoorthi <spoorthi.s@proenco.com>
2018-09-18 17:37:01 +0530

    removing unnecessary references and unused namespaces

--- Jettison/JettisonClassLibrary/FileOperationAPIWrapper.cs
+++ Jettison/JettisonClassLibrary/FileOperationAPIWrapper.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using IWshRuntimeLibrary;
 
 namespace JettisonClassLibrary
-{   
+{
 
     public class FileOperationAPIWrapper
     {
